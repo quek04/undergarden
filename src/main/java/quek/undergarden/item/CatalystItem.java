97e78d745002fac7d43d3be138c9958ff0574c78
quek <55364841+quek04@users.noreply.github.com>
2020-06-28 13:51:41 -0500

    fixed portal activation being strange

--- src/main/java/quek/undergarden/item/CatalystItem.java
+++ src/main/java/quek/undergarden/item/CatalystItem.java
@@ -3,6 +3,8 @@ package quek.undergarden.item;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import quek.undergarden.block.UndergardenPortalBlock;
 import quek.undergarden.registry.UndergardenBlocks;
 import quek.undergarden.registry.UndergardenItemGroups;
@@ -17,10 +19,14 @@ public class CatalystItem extends Item {
 
     @Override
     public ActionResultType onItemUse(ItemUseContext context) {
-        if(((UndergardenPortalBlock)UndergardenBlocks.undergarden_portal.get()).trySpawnPortal(context.getWorld(), context.getPos())) {
-            return ActionResultType.CONSUME;
+        for(Direction direction : Direction.Plane.VERTICAL) {
+            BlockPos framePos = context.getPos().offset(direction);
+            if(((UndergardenPortalBlock)UndergardenBlocks.undergarden_portal.get()).trySpawnPortal(context.getWorld(), framePos)) {
+                return ActionResultType.CONSUME;
+            }
+            else return ActionResultType.FAIL;
         }
-        else return ActionResultType.FAIL;
+        return ActionResultType.FAIL;
     }
 
 }
--- src/main/java/quek/undergarden/item/DepthrockPebbleItem.java
+++ src/main/java/quek/undergarden/item/DepthrockPebbleItem.java
@@ -1,10 +1,6 @@
 package quek.undergarden.item;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArrowItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-import quek.undergarden.entity.projectile.SlingshotAmmoEntity;
 import quek.undergarden.registry.UndergardenItemGroups;
 
 public class DepthrockPebbleItem extends ArrowItem {
