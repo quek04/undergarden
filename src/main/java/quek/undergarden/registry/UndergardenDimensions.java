47b4ff62572eeb7665b122f6ad10444022b4994c
quek <55364841+quek04@users.noreply.github.com>
2020-08-04 17:58:49 -0500

    oh yeah that too

--- src/main/java/quek/undergarden/registry/UndergardenDimensions.java
+++ src/main/java/quek/undergarden/registry/UndergardenDimensions.java
@@ -15,26 +15,5 @@ public class UndergardenDimensions {
     public static final RegistryKey<DimensionType> otherside = RegistryKey.func_240903_a_(Registry.DIMENSION_TYPE_KEY, new ResourceLocation(UndergardenMod.MODID, "otherside"));
     public static final RegistryKey<World> otherside_w = RegistryKey.func_240903_a_(Registry.WORLD_KEY, new ResourceLocation(UndergardenMod.MODID, "otherside"));
 
-    /* WHAT DO I DO WITH THIS
-    public static final DeferredRegister<ModDimension> MOD_DIMENSIONS = DeferredRegister.create(ForgeRegistries.MOD_DIMENSIONS, UndergardenMod.MODID);
-
-    public static DimensionType undergarden_dimension;
-    public static DimensionType otherside_dimension;
-
-    public static final RegistryObject<ModDimension> UNDERGARDEN = MOD_DIMENSIONS.register("undergarden", () -> new ModDimension() {
-        @Override
-        public BiFunction<World, DimensionType, ? extends Dimension> getFactory() {
-            return UndergardenDimension::new;
-        }
-    });
-
-    public static final RegistryObject<ModDimension> OTHERSIDE = MOD_DIMENSIONS.register("otherside", () -> new ModDimension() {
-        @Override
-        public BiFunction<World, DimensionType, ? extends Dimension> getFactory() {
-            return OthersideDimension::new;
-        }
-    });
-    */
-
 }
 
