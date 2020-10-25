70543e1fd1ef3b7b13aec99fb38580e4c0a5974e
quek <55364841+quek04@users.noreply.github.com>
2020-10-25 16:13:46 -0500

    fixed stem behavior

--- src/main/java/quek/undergarden/block/world/UGStemBlock.java
+++ src/main/java/quek/undergarden/block/world/UGStemBlock.java
@@ -5,16 +5,11 @@ import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
-import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -27,7 +22,6 @@ import java.util.Random;
 public class UGStemBlock extends StemBlock {
 
     public static final IntegerProperty AGE = BlockStateProperties.AGE_0_7;
-    protected static final VoxelShape[] SHAPES = new VoxelShape[]{Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 2.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 4.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 6.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 8.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 10.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 12.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 14.0D, 9.0D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 16.0D, 9.0D)};
     private final StemGrownBlock crop;
 
     public UGStemBlock(StemGrownBlock crop) {
@@ -54,21 +48,20 @@ public class UGStemBlock extends StemBlock {
     }
 
     @Override
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        super.tick(state, worldIn, pos, rand);
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
         if (!worldIn.isAreaLoaded(pos, 1))
             return; // Forge: prevent loading unloaded chunks when checking neighbor's light
-        float f = UGCropsBlock.getGrowthChance(this, worldIn, pos);
-        if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt((int) (25.0F / f) + 1) == 0)) {
+        float f = CropsBlock.getGrowthChance(this, worldIn, pos);
+        if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
             int i = state.get(AGE);
             if (i < 7) {
                 worldIn.setBlockState(pos, state.with(AGE, i + 1), 2);
             } else {
-                Direction direction = Direction.Plane.HORIZONTAL.random(rand);
+                Direction direction = Direction.Plane.HORIZONTAL.random(random);
                 BlockPos blockpos = pos.offset(direction);
-                BlockState soil = worldIn.getBlockState(blockpos.down());
-                Block block = soil.getBlock();
-                if (worldIn.isAirBlock(blockpos) && (soil.canSustainPlant(worldIn, blockpos.down(), Direction.UP, this) || block == UGBlocks.deepsoil_farmland.get())) {
+                BlockState blockstate = worldIn.getBlockState(blockpos.down());
+                Block block = blockstate.getBlock();
+                if (worldIn.isAirBlock(blockpos) && (blockstate.canSustainPlant(worldIn, blockpos.down(), Direction.UP, this) || block == UGBlocks.deepsoil_farmland.get() || block == UGBlocks.deepsoil.get() || block == UGBlocks.coarse_deepsoil.get() || block == UGBlocks.deepturf_block.get())) {
                     worldIn.setBlockState(blockpos, this.crop.getDefaultState());
                     worldIn.setBlockState(pos, this.crop.getAttachedStem().getDefaultState().with(HorizontalBlock.HORIZONTAL_FACING, direction));
                 }
