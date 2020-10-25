da93f61b1def4eebadc01cfb7ea4e5aadda131de
quek <55364841+quek04@users.noreply.github.com>
2020-10-25 13:01:45 -0500

    cleaned up shields

--- src/main/java/quek/undergarden/item/tool/UndergardenShieldItem.java
+++ src/main/java/quek/undergarden/item/tool/UndergardenShieldItem.java
@@ -2,18 +2,12 @@ package quek.undergarden.item.tool;
 
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Mod;
 import quek.undergarden.registry.UGItemGroups;
 import quek.undergarden.registry.UGShields;
 
 import javax.annotation.Nullable;
 
-@Mod.EventBusSubscriber
 public class UndergardenShieldItem extends ShieldItem {
 
     private final UGShields shieldTiers;
@@ -40,29 +34,12 @@ public class UndergardenShieldItem extends ShieldItem {
     }
 
     @Override
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.BLOCK;
-    }
-
-    @Override
-    public int getUseDuration(ItemStack stack) {
-        return 72000;
-    }
-
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        ItemStack itemstack = playerIn.getHeldItem(handIn);
-        playerIn.setActiveHand(handIn);
-        return ActionResult.resultConsume(itemstack);
-    }
-
-    @Override
     public boolean isShield(ItemStack stack, @Nullable LivingEntity entity) {
         return true;
     }
 
     @Override
     public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
-        return this.shieldTiers.getRepairMaterial().test(repair) || super.getIsRepairable(toRepair, repair);
+        return this.shieldTiers.getRepairMaterial().test(repair);
     }
 }
