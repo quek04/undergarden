261f9a35a6b683d82a161db85941a43522c0eb90
quek <55364841+quek04@users.noreply.github.com>
2020-10-31 22:12:58 -0500

    fixed tall plants

--- src/main/java/quek/undergarden/block/UGDoublePlantBlock.java
+++ src/main/java/quek/undergarden/block/UGDoublePlantBlock.java
@@ -4,10 +4,8 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DoublePlantBlock;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
 import quek.undergarden.registry.UGBlocks;
 
 public class UGDoublePlantBlock extends DoublePlantBlock {
@@ -19,13 +17,5 @@ public class UGDoublePlantBlock extends DoublePlantBlock {
     public boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
         Block block = state.getBlock();
         return block == UGBlocks.deepturf_block.get() || block == UGBlocks.deepsoil.get();
-    }
-
-    @Override
-    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-        BlockPos blockpos = pos.down();
-        if (state.getBlock() == this) //Forge: This function is called during world gen and placement, before this block is set, so if we are not 'here' then assume it's the pre-check.
-            return worldIn.getBlockState(blockpos).canSustainPlant(worldIn, blockpos, Direction.UP, this);
-        return this.isValidGround(worldIn.getBlockState(blockpos), worldIn, blockpos);
     }
 }
