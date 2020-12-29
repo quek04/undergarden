fb72ea07d25c17eddc3f731c98b48a0495625e70
quek <55364841+quek04@users.noreply.github.com>
2020-12-29 14:28:08 -0600

    added limax maximus texture & model updated goo ball texture

--- src/main/java/quek/undergarden/data/UGItemModels.java
+++ src/main/java/quek/undergarden/data/UGItemModels.java
@@ -118,6 +118,7 @@ public class UGItemModels extends UGItemModelProvider {
         itemBlock(UGBlocks.CHISELED_SHIVERSTONE_BRICKS);
 
         normalItem(UGItems.MAMMOTH_DISC);
+        normalItem(UGItems.LIMAX_MAXIMUS_DISC);
         normalItem(UGItems.CATALYST);
         normalItem(UGItems.DEPTHROCK_PEBBLE);
         normalItem(UGItems.TWISTYTWIG);
