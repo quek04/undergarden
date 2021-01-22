ca733f3f25eab7eeb87bd04bbc97dc286b202edc
quek <55364841+quek04@users.noreply.github.com>
2021-01-21 18:04:46 -0600

    fixed seed getting set back to 0 because im an idiot

--- src/main/java/quek/undergarden/mixin/DimensionGeneratorSettingsMixin.java
+++ src/main/java/quek/undergarden/mixin/DimensionGeneratorSettingsMixin.java
@@ -9,11 +9,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import quek.undergarden.world.dimension.SeedBearer;
 
+import java.util.Optional;
+
 @Mixin(DimensionGeneratorSettings.class)
 public class DimensionGeneratorSettingsMixin {
 
-    @Inject(method = "<init>(JZZLnet/minecraft/util/registry/SimpleRegistry;)V", at = @At(value = "RETURN"))
-    private void getSeedFromConstructor(long seed, boolean generateFeatures, boolean bonusChest, SimpleRegistry<Dimension> registry, CallbackInfo ci) {
+    @Inject(method = "<init>(JZZLnet/minecraft/util/registry/SimpleRegistry;Ljava/util/Optional;)V", at = @At(value = "RETURN"))
+    private void getSeedFromConstructor(long seed, boolean generateFeatures, boolean bonusChest, SimpleRegistry<Dimension> options, Optional<String> legacyOptions, CallbackInfo ci) {
         SeedBearer.putInSeed(seed);
     }
 }
