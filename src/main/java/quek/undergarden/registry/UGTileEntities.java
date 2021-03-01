742ebca0a7e6716a7138be40da5544cb3d0258a5
quek <55364841+quek04@users.noreply.github.com>
2021-03-01 17:23:41 -0600

    made use of TileEntityType.Builder instead of the constructor

--- src/main/java/quek/undergarden/registry/UGTileEntities.java
+++ src/main/java/quek/undergarden/registry/UGTileEntities.java
@@ -1,6 +1,5 @@
 package quek.undergarden.registry;
 
-import com.google.common.collect.Sets;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,7 +13,7 @@ public class UGTileEntities {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, UGMod.MODID);
 
     public static final RegistryObject<TileEntityType<ShardTorchTE>> SHARD_TORCH = TILE_ENTITIES.register("shard_torch_te", () ->
-            new TileEntityType<>(ShardTorchTE::new, Sets.newHashSet(UGBlocks.SHARD_TORCH.get(), UGBlocks.SHARD_WALL_TORCH.get(), UGBlocks.SHARD_O_LANTERN.get()), null));
+            TileEntityType.Builder.create(ShardTorchTE::new, UGBlocks.SHARD_TORCH.get(), UGBlocks.SHARD_WALL_TORCH.get(), UGBlocks.SHARD_O_LANTERN.get()).build(null));
     public static final RegistryObject<TileEntityType<SmogVentTE>> SMOG_VENT = TILE_ENTITIES.register("smog_vent_te", () ->
-            new TileEntityType<>(SmogVentTE::new, Sets.newHashSet(UGBlocks.SMOG_VENT.get()), null));
+            TileEntityType.Builder.create(SmogVentTE::new, UGBlocks.SMOG_VENT.get()).build(null));
 }
