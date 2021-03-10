0314b7c9c3fe84f2851d3928c23b68eb92fd8fb8
quek <55364841+quek04@users.noreply.github.com>
2021-03-10 13:28:12 -0600

    changed guardian spawn egg color

--- src/main/java/quek/undergarden/registry/UGItems.java
+++ src/main/java/quek/undergarden/registry/UGItems.java
@@ -152,5 +152,5 @@ public class UGItems {
     public static final RegistryObject<UGSpawnEggItem> GWIB_SPAWN_EGG = ITEMS.register("gwib_spawn_egg", () -> new UGSpawnEggItem(UGEntityTypes.GWIB_TYPE, 10064737, 15845330));
 
     public static final RegistryObject<Item> MASTICATOR_SPAWN_EGG = ITEMS.register("masticator_spawn_egg", () -> new UGSpawnEggItem(UGEntityTypes.MASTICATOR_TYPE, 2366466, 15881511));
-    public static final RegistryObject<Item> FORGOTTEN_GUARDIAN_SPAWN_EGG = ITEMS.register("forgotten_guardian_spawn_egg", () -> new UGSpawnEggItem(UGEntityTypes.FORGOTTEN_GUARDIAN_TYPE, 2114606, 1962863));
+    public static final RegistryObject<Item> FORGOTTEN_GUARDIAN_SPAWN_EGG = ITEMS.register("forgotten_guardian_spawn_egg", () -> new UGSpawnEggItem(UGEntityTypes.FORGOTTEN_GUARDIAN_TYPE, 4076864, 9951919));
 }
