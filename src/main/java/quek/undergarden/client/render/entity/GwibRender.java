2366c8649a1f5e34f417663fa34c90ef7045e625
quek <55364841+quek04@users.noreply.github.com>
2021-04-17 21:01:44 -0500

    no gwib rotate

--- src/main/java/quek/undergarden/client/render/entity/GwibRender.java
+++ src/main/java/quek/undergarden/client/render/entity/GwibRender.java
@@ -31,9 +31,5 @@ public class GwibRender extends MobRenderer<GwibEntity, GwibModel<GwibEntity>> {
         super.setupRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
         float f = 4.3F * MathHelper.sin(0.6F * ageInTicks);
         matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(f));
-        if (!entityLiving.isInWater()) {
-            matrixStackIn.translate(0.1F, 0.1F, -0.1F);
-            matrixStackIn.mulPose(Vector3f.ZP.rotationDegrees(90.0F));
-        }
     }
 }
