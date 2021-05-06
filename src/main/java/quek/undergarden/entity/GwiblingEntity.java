044625af2d6d16406d3312170af09e5cc69d6068
quek <55364841+quek04@users.noreply.github.com>
2021-05-05 19:19:17 -0500

    gwiblings hopefully avoid gwibs

--- src/main/java/quek/undergarden/entity/GwiblingEntity.java
+++ src/main/java/quek/undergarden/entity/GwiblingEntity.java
@@ -3,6 +3,7 @@ package quek.undergarden.entity;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.entity.passive.fish.AbstractFishEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
@@ -19,6 +20,12 @@ public class GwiblingEntity extends AbstractFishEntity {
 
     public GwiblingEntity(EntityType<? extends AbstractFishEntity> type, World worldIn) {
         super(type, worldIn);
+    }
+
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(2, new AvoidEntityGoal<>(this, GwibEntity.class, 8.0F, 1.6D, 1.4D));
     }
 
     public static boolean canGwiblingSpawn(EntityType<? extends AbstractFishEntity> type, IWorld worldIn, SpawnReason reason, BlockPos pos, Random randomIn) {
