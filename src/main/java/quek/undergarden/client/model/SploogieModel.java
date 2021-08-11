66fccab17c2cecbcfea28bf36a2bcd6240f7d7d3
quek <55364841+quek04@users.noreply.github.com>
2021-08-11 09:43:02 -0500

    new sploogie model & texture

--- src/main/java/quek/undergarden/client/model/SploogieModel.java
+++ src/main/java/quek/undergarden/client/model/SploogieModel.java
@@ -1,28 +1,21 @@
 package quek.undergarden.client.model;
-// Made with Blockbench 3.7.4
-// Exported for Minecraft version 1.15
 
 import com.google.common.collect.ImmutableList;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.SegmentedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import quek.undergarden.entity.cavern.SploogieEntity;
 
-@OnlyIn(Dist.CLIENT)
 public class SploogieModel<T extends SploogieEntity> extends SegmentedModel<T> {
 	private final ModelRenderer head;
-	private final ModelRenderer snout;
-	private final ModelRenderer body;
-	private final ModelRenderer leftLeg1;
-	private final ModelRenderer leftLeg2;
-	private final ModelRenderer leftLeg3;
-	private final ModelRenderer rightLeg1;
-	private final ModelRenderer rightLeg2;
-	private final ModelRenderer rightLeg3;
+	private final ModelRenderer spine1;
+	private final ModelRenderer mouth;
+	private final ModelRenderer bodySegment1;
+	private final ModelRenderer spine2;
+	private final ModelRenderer bodySegment2;
+	private final ModelRenderer spine3;
+	private final ModelRenderer bodySegment3;
+	private final ModelRenderer spine4;
 
 	public SploogieModel() {
 		texWidth = 64;
@@ -30,49 +23,66 @@ public class SploogieModel<T extends SploogieEntity> extends SegmentedModel<T> {
 
 		head = new ModelRenderer(this);
 		head.setPos(0.0F, 20.0F, 0.0F);
-		head.texOffs(0, 0).addBox(-5.0F, -10.0F, -5.0F, 10.0F, 10.0F, 10.0F, 0.0F, false);
-		head.texOffs(0, 20).addBox(-4.0F, -9.0F, -6.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-		head.texOffs(0, 20).addBox(1.0F, -9.0F, -6.0F, 3.0F, 3.0F, 1.0F, 0.0F, true);
+		head.texOffs(0, 30).addBox(4.0F, -2.0F, -9.0F, 4.0F, 4.0F, 4.0F, 0.0F, false);
+		head.texOffs(0, 30).addBox(-8.0F, -2.0F, -9.0F, 4.0F, 4.0F, 4.0F, 0.0F, true);
+		head.texOffs(0, 0).addBox(-5.0F, -4.0F, -8.0F, 10.0F, 8.0F, 8.0F, 0.0F, false);
 
-		snout = new ModelRenderer(this);
-		snout.setPos(0.0F, 4.0F, 0.0F);
-		head.addChild(snout);
-		snout.texOffs(0, 36).addBox(-1.0F, -9.0F, -7.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
-		snout.texOffs(0, 31).addBox(-2.0F, -10.0F, -8.0F, 4.0F, 4.0F, 1.0F, 0.0F, false);
+		//spine1 = new ModelRenderer(this);
+		//spine1.setPos(0.0F, -4.0F, -5.0F);
+		//head.addChild(spine1);
 
-		body = new ModelRenderer(this);
-		body.setPos(0.0F, 24.0F, 0.0F);
-		body.texOffs(0, 20).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 3.0F, 8.0F, 0.0F, false);
+		spine1 = new ModelRenderer(this);
+		spine1.setPos(0.0F, -4.0F, -5.0F);
+		head.addChild(spine1);
+		setRotationAngle(spine1, -0.7854F, 0.0F, 0.0F);
+		spine1.texOffs(0, 0).addBox(-2.0F, -7.0F, 0.0F, 4.0F, 7.0F, 0.0F, 0.0F, false);
 
-		leftLeg1 = new ModelRenderer(this);
-		leftLeg1.setPos(4.0F, 22.0F, -3.0F);
-		setRotationAngle(leftLeg1, 0.0F, 0.0F, 0.6109F);
-		leftLeg1.texOffs(30, 8).addBox(-1.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, false);
+		mouth = new ModelRenderer(this);
+		mouth.setPos(0.0F, 4.0F, -8.0F);
+		head.addChild(mouth);
+		mouth.texOffs(12, 34).addBox(-2.0F, -5.0F, -4.0F, 4.0F, 4.0F, 4.0F, 0.0F, false);
 
-		leftLeg2 = new ModelRenderer(this);
-		leftLeg2.setPos(4.0F, 22.0F, -1.0F);
-		setRotationAngle(leftLeg2, 0.0F, 0.0F, 0.6109F);
-		leftLeg2.texOffs(30, 8).addBox(-1.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, false);
+		bodySegment1 = new ModelRenderer(this);
+		bodySegment1.setPos(0.0F, 21.0F, 0.0F);
+		bodySegment1.texOffs(0, 16).addBox(-4.0F, -4.0F, 0.0F, 8.0F, 7.0F, 7.0F, 0.0F, false);
 
-		leftLeg3 = new ModelRenderer(this);
-		leftLeg3.setPos(4.0F, 22.0F, 1.0F);
-		setRotationAngle(leftLeg3, 0.0F, 0.0F, 0.6109F);
-		leftLeg3.texOffs(30, 8).addBox(-1.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, false);
+		//spine2 = new ModelRenderer(this);
+		//spine2.setPos(0.0F, -4.0F, 3.0F);
+		//segment1.addChild(spine2);
 
-		rightLeg1 = new ModelRenderer(this);
-		rightLeg1.setPos(-4.0F, 22.0F, -3.0F);
-		setRotationAngle(rightLeg1, 0.0F, 0.0F, -0.6109F);
-		rightLeg1.texOffs(30, 8).addBox(-4.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, true);
+		spine2 = new ModelRenderer(this);
+		spine2.setPos(0.0F, -4.0F, 3.0F);
+		bodySegment1.addChild(spine2);
+		setRotationAngle(spine2, -0.9599F, 0.0F, 0.0F);
+		spine2.texOffs(0, 16).addBox(-1.5F, -6.0F, 0.0F, 3.0F, 6.0F, 0.0F, 0.0F, false);
 
-		rightLeg2 = new ModelRenderer(this);
-		rightLeg2.setPos(-4.0F, 22.0F, -1.0F);
-		setRotationAngle(rightLeg2, 0.0F, 0.0F, -0.6109F);
-		rightLeg2.texOffs(30, 8).addBox(-4.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, true);
+		bodySegment2 = new ModelRenderer(this);
+		bodySegment2.setPos(0.0F, 21.0F, 7.0F);
+		bodySegment2.texOffs(24, 24).addBox(-3.0F, -3.0F, 0.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
 
-		rightLeg3 = new ModelRenderer(this);
-		rightLeg3.setPos(-4.0F, 22.0F, 1.0F);
-		setRotationAngle(rightLeg3, 0.0F, 0.0F, -0.6109F);
-		rightLeg3.texOffs(30, 8).addBox(-4.0F, -1.0F, 0.0F, 5.0F, 1.0F, 1.0F, 0.0F, true);
+		//spine3 = new ModelRenderer(this);
+		//spine3.setPos(0.0F, -3.0F, 3.0F);
+		//segment2.addChild(spine3);
+
+		spine3 = new ModelRenderer(this);
+		spine3.setPos(0.0F, -3.0F, 3.0F);
+		bodySegment2.addChild(spine3);
+		setRotationAngle(spine3, -1.1345F, 0.0F, 0.0F);
+		spine3.texOffs(23, 16).addBox(-1.0F, -4.0F, 0.0F, 2.0F, 4.0F, 0.0F, 0.0F, false);
+
+		bodySegment3 = new ModelRenderer(this);
+		bodySegment3.setPos(0.0F, 22.0F, 13.0F);
+		bodySegment3.texOffs(30, 16).addBox(-2.0F, -2.0F, 0.0F, 4.0F, 4.0F, 4.0F, 0.0F, false);
+
+		//spine4 = new ModelRenderer(this);
+		//spine4.setPos(0.0F, -2.0F, 2.0F);
+		//segment3.addChild(spine4);
+
+		spine4 = new ModelRenderer(this);
+		spine4.setPos(0.0F, -2.0F, 2.0F);
+		bodySegment3.addChild(spine4);
+		setRotationAngle(spine4, -1.309F, 0.0F, 0.0F);
+		spine4.texOffs(23, 20).addBox(-0.5F, -3.0F, 0.0F, 1.0F, 3.0F, 0.0F, 0.0F, false);
 	}
 
 	@Override
@@ -80,30 +90,20 @@ public class SploogieModel<T extends SploogieEntity> extends SegmentedModel<T> {
 		this.head.yRot = netHeadYaw * ((float)Math.PI / 180F);
 		this.head.xRot = headPitch * ((float)Math.PI / 180F);
 
-		this.leftLeg1.zRot = 0.6109F + MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-		this.leftLeg2.zRot = 0.6109F + MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-		this.leftLeg3.zRot = 0.6109F + MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		this.head.x = MathHelper.cos(ageInTicks * 0.9F + 2 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1);
 
-		this.rightLeg1.zRot = -0.6109F + MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-		this.rightLeg2.zRot = -0.6109F + MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-		this.rightLeg3.zRot = -0.6109F + MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-	}
+		this.bodySegment1.yRot = MathHelper.cos(ageInTicks * 0.9F + 2 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1);
+		this.bodySegment2.yRot = MathHelper.cos(ageInTicks * 0.9F + 1 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1 + Math.abs(1 - 2));
+		this.bodySegment3.yRot = MathHelper.cos(ageInTicks * 0.9F + 0 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1 + Math.abs(-2));
 
-	@Override
-	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-		head.render(matrixStack, buffer, packedLight, packedOverlay);
-		body.render(matrixStack, buffer, packedLight, packedOverlay);
-		leftLeg1.render(matrixStack, buffer, packedLight, packedOverlay);
-		leftLeg2.render(matrixStack, buffer, packedLight, packedOverlay);
-		leftLeg3.render(matrixStack, buffer, packedLight, packedOverlay);
-		rightLeg1.render(matrixStack, buffer, packedLight, packedOverlay);
-		rightLeg2.render(matrixStack, buffer, packedLight, packedOverlay);
-		rightLeg3.render(matrixStack, buffer, packedLight, packedOverlay);
+		this.bodySegment1.x = MathHelper.cos(ageInTicks * 0.9F + 2 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1);
+		this.bodySegment2.x = MathHelper.cos(ageInTicks * 0.9F + 1 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1 + Math.abs(1 - 2));
+		this.bodySegment3.x = MathHelper.cos(ageInTicks * 0.9F + 0 * 0.15F * (float)Math.PI) * (float)Math.PI * 0.05F * (1 + Math.abs(-2));
 	}
 
 	@Override
 	public Iterable<ModelRenderer> parts() {
-		return ImmutableList.of(this.head, this.body, this.leftLeg1, this.leftLeg2, this.leftLeg3, this.rightLeg1, this.rightLeg2, this.rightLeg3);
+		return ImmutableList.of(this.head, this.bodySegment1, this.bodySegment2, this.bodySegment3);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
