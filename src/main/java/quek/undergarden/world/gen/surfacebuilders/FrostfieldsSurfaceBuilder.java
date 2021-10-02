3057737e7413cd0d238d30fc67c326e1c094131e
quek <55364841+quek04@users.noreply.github.com>
2021-10-01 19:28:42 -0500

    fix frostfields surface builder

--- src/main/java/quek/undergarden/world/gen/surfacebuilders/FrostfieldsSurfaceBuilder.java
+++ src/main/java/quek/undergarden/world/gen/surfacebuilders/FrostfieldsSurfaceBuilder.java
@@ -20,63 +20,93 @@ public class FrostfieldsSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderBase
 
     @Override
     public void apply(Random pRandom, ChunkAccess pChunk, Biome pBiome, int pX, int pZ, int pHeight, double pNoise, BlockState pDefaultBlock, BlockState pDefaultFluid, int pSeaLevel, int pMinSurfaceLevel, long pSeed, SurfaceBuilderBaseConfiguration pConfig) {
-
+        this.surfaceBuilder(pRandom, pChunk, pBiome, pX, pZ, pHeight, pNoise, pDefaultBlock, pDefaultFluid, pConfig.getTopMaterial(), pConfig.getUnderMaterial(), pConfig.getUnderwaterMaterial(), pSeaLevel, pMinSurfaceLevel);
     }
 
-    protected void apply(Random pRandom, ChunkAccess pChunk, Biome pBiome, int pX, int pZ, int pHeight, double pNoise, BlockState pDefaultBlock, BlockState pDefaultFluid, BlockState pTopMaterial, BlockState pUnderMaterial, BlockState pUnderwaterMaterial, int pSeaLevel, int pMinSurfaceLevel) {
-        BlockState topBlock = pTopMaterial;
-        BlockState middleBlock = pUnderMaterial;
+    protected void surfaceBuilder(Random pRandom, ChunkAccess pChunk, Biome pBiome, int pX, int pZ, int pHeight, double pNoise, BlockState pDefaultBlock, BlockState pDefaultFluid, BlockState pTopMaterial, BlockState pUnderMaterial, BlockState pUnderwaterMaterial, int pSeaLevel, int pMinSurfaceLevel) {
         BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
-        int i = -1;
-        int basin = (int)(pNoise / 3.0D + 3.0D + pRandom.nextDouble() * 0.25D);
-        int xPos = pX & 15;
-        int zPos = pZ & 15;
+        //replaces all default blocks (stone in Overworld, Depthrock in Undergarden etc) with Shiverstone
+        if(pChunk.getBlockState(pos).is(pDefaultBlock.getBlock())) {
+            pChunk.setBlockState(pos, UGBlocks.SHIVERSTONE.get().defaultBlockState(), false);
+        }
+        //replaces all default fluids with ice
+        if (pChunk.getBlockState(pos).is(pDefaultFluid.getBlock())) {
+            pChunk.setBlockState(pos, Blocks.ICE.defaultBlockState(), false);
+        }
+        int i = (int)(pNoise / 3.0D + 3.0D + pRandom.nextDouble() * 0.25D);
+        if (i == 0) {
+            boolean flag = false;
 
-        for(int y = pHeight; y >= 0; --y) {
-            pos.set(xPos, y, zPos);
-            BlockState blockstate2 = pChunk.getBlockState(pos);
-            if (blockstate2.isAir()) {
-                i = -1;
-            }
-            else if (blockstate2.is(pDefaultBlock.getBlock())) {
-                if (i == -1) {
-                    if (basin <= 0) {
-                        topBlock = Blocks.AIR.defaultBlockState();
-                        middleBlock = pDefaultBlock;
-                    } else if (y >= pSeaLevel - 4 && y <= pSeaLevel + 1) {
-                        topBlock = pTopMaterial;
-                        middleBlock = pUnderMaterial;
+            for(int height = pHeight; height >= pMinSurfaceLevel; --height) {
+                pos.set(pX, height, pZ);
+                BlockState blockstate = pChunk.getBlockState(pos);
+                if (blockstate.isAir()) {
+                    flag = false;
+                }
+                else if (blockstate.is(pDefaultBlock.getBlock())) {
+                    if (!flag) {
+                        BlockState blockstate1;
+                        if (height >= pSeaLevel) {
+                            blockstate1 = Blocks.AIR.defaultBlockState();
+                        }
+                        else if (height == pSeaLevel - 1) {
+                            blockstate1 = pBiome.getTemperature(pos) < 0.15F ? Blocks.ICE.defaultBlockState() : pDefaultFluid;
+                        }
+                        else if (height >= pSeaLevel - (7 + i)) {
+                            blockstate1 = pDefaultBlock;
+                        }
+                        else {
+                            blockstate1 = pUnderwaterMaterial;
+                        }
+
+                        pChunk.setBlockState(pos, blockstate1, false);
                     }
 
-                    if (y < pSeaLevel && (topBlock == null || topBlock.isAir())) {
-                        topBlock = Blocks.ICE.defaultBlockState();
-                        pos.set(xPos, y, zPos);
-                    }
-
-                    i = basin;
-                    if (y >= pSeaLevel - 1) {
-                        pChunk.setBlockState(pos, topBlock, false);
-                    } else if (y < pSeaLevel - 7 - basin) {
-                        topBlock = Blocks.AIR.defaultBlockState();
-                        middleBlock = pDefaultBlock;
-                        pChunk.setBlockState(pos, bottom, false);
-                    } else {
-                        pChunk.setBlockState(pos, middleBlock, false);
-                    }
-                } else if (i > 0) {
-                    --i;
-                    pChunk.setBlockState(pos, middleBlock, false);
+                    flag = true;
                 }
             }
-            //replaces all default blocks (stone in Overworld, Depthrock in Undergarden etc) with Shiverstone
-            if(pChunk.getBlockState(pos).is(pDefaultBlock.getBlock())) {
-                pChunk.setBlockState(pos, UGBlocks.SHIVERSTONE.get().defaultBlockState(), false);
-            }
-            //replaces all water with ice
-            if (pChunk.getBlockState(pos).is(pDefaultFluid.getBlock())) {
-                pChunk.setBlockState(pos, Blocks.ICE.defaultBlockState(), false);
+        }
+        else {
+            BlockState underMaterial = pUnderMaterial;
+            int k = -1;
+
+            for(int height = pHeight; height >= pMinSurfaceLevel; --height) {
+                pos.set(pX, height, pZ);
+                BlockState blockstate4 = pChunk.getBlockState(pos);
+                if (blockstate4.isAir()) {
+                    k = -1;
+                }
+                else if (blockstate4.is(pDefaultBlock.getBlock())) {
+                    if (k == -1) {
+                        k = i;
+                        BlockState blockstate2;
+                        if (height >= pSeaLevel + 2) {
+                            blockstate2 = pTopMaterial;
+                        }
+                        else if (height >= pSeaLevel - 1) {
+                            underMaterial = pUnderMaterial;
+                            blockstate2 = pTopMaterial;
+                        }
+                        else if (height >= pSeaLevel - 4) {
+                            underMaterial = pUnderMaterial;
+                            blockstate2 = pUnderMaterial;
+                        }
+                        else if (height >= pSeaLevel - (7 + i)) {
+                            blockstate2 = underMaterial;
+                        }
+                        else {
+                            underMaterial = pDefaultBlock;
+                            blockstate2 = pUnderwaterMaterial;
+                        }
+
+                        pChunk.setBlockState(pos, blockstate2, false);
+                    }
+                    else if (k > 0) {
+                        --k;
+                        pChunk.setBlockState(pos, underMaterial, false);
+                    }
+                }
             }
         }
-
     }
 }
