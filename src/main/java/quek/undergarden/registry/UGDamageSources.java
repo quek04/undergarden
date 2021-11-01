0813359b6c743a8d9b57c0dab4df5a713128bfb4
quek <55364841+quek04@users.noreply.github.com>
2021-11-01 14:54:34 -0500

    delete unused frozen deepturf DamageSource

--- src/main/java/quek/undergarden/registry/UGDamageSources.java
+++ src/main/java/quek/undergarden/registry/UGDamageSources.java
@@ -4,7 +4,6 @@ import net.minecraft.world.damagesource.DamageSource;
 
 public class UGDamageSources {
 
-    public static final DamageSource FROZEN_DEEPTURF = new DamageSource("frozen_deepturf");
     public static final DamageSource BLISTERBERRY_BUSH = new DamageSource("blisterberry_bush");
     public static final DamageSource SHARD_TORCH = new DamageSource("shard_torch").bypassArmor().setMagic();
 }
