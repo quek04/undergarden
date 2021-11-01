f0209966d2be089b6fc692146dbf5465d0c6b8d9
quek <55364841+quek04@users.noreply.github.com>
2021-11-01 15:07:37 -0500

    forgot to fix RottenBlisterberryItem

--- src/main/java/quek/undergarden/item/RottenBlisterberryItem.java
+++ src/main/java/quek/undergarden/item/RottenBlisterberryItem.java
@@ -1,18 +1,16 @@
 package quek.undergarden.item;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.level.Level;
 import quek.undergarden.entity.projectile.RottenBlisterberryEntity;
 import quek.undergarden.registry.UGItemGroups;
-
-import net.minecraft.world.item.Item.Properties;
 
 public class RottenBlisterberryItem extends Item {
 
@@ -24,19 +22,19 @@ public class RottenBlisterberryItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-        ItemStack itemstack = playerIn.getItemInHand(handIn);
-        worldIn.playSound(null, playerIn.getX(), playerIn.getY(), playerIn.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-        playerIn.getCooldowns().addCooldown(this, 30);
-        if (!worldIn.isClientSide) {
-            RottenBlisterberryEntity berry = new RottenBlisterberryEntity(worldIn, playerIn);
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+        player.getCooldowns().addCooldown(this, 30);
+        if (!level.isClientSide) {
+            RottenBlisterberryEntity berry = new RottenBlisterberryEntity(level, player);
             berry.setItem(itemstack);
-            berry.shootFromRotation(playerIn, playerIn.xRot, playerIn.yRot, 0.0F, 1.5F, 1.0F);
-            worldIn.addFreshEntity(berry);
+            berry.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+            level.addFreshEntity(berry);
         }
 
-        playerIn.awardStat(Stats.ITEM_USED.get(this));
-        if (!playerIn.abilities.instabuild) {
+        player.awardStat(Stats.ITEM_USED.get(this));
+        if (!player.getAbilities().instabuild) {
             itemstack.shrink(1);
         }
 
