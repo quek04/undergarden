af425cfbd7d4555828f9120d4ed39f3cb49b0b24
quek <55364841+quek04@users.noreply.github.com>
2021-11-24 11:01:42 -0600

    nerfed brittleness

--- src/main/java/quek/undergarden/potion/BrittlenessEffect.java
+++ src/main/java/quek/undergarden/potion/BrittlenessEffect.java
@@ -1,9 +1,9 @@
 package quek.undergarden.potion;
 
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,7 +23,7 @@ public class BrittlenessEffect extends MobEffect {
         float damage = event.getAmount();
 
         if (entity.hasEffect(UGEffects.BRITTLENESS.get()) && source != DamageSource.OUT_OF_WORLD) {
-            int amplifier = (entity.getEffect(UGEffects.BRITTLENESS.get()).getAmplifier() + 1) + (entity.getArmorValue() / 2) * 2;
+            int amplifier = (entity.getEffect(UGEffects.BRITTLENESS.get()).getAmplifier() + 1) + (entity.getArmorValue() / 4) * 2;
 
             event.setAmount(damage + amplifier);
         }
