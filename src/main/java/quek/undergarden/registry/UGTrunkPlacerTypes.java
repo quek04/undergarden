12b51c0dd303f632919ff21bb7ecdaa6370369f0
quek <55364841+quek04@users.noreply.github.com>
2021-12-18 19:22:02 -0600

    remove GrongleTrunkPlacer as it isnt used

--- src/main/java/quek/undergarden/registry/UGTrunkPlacerTypes.java
+++ src/main/java/quek/undergarden/registry/UGTrunkPlacerTypes.java
@@ -6,17 +6,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacerType;
 import quek.undergarden.Undergarden;
-import quek.undergarden.world.gen.trunkplacer.GrongleTrunkPlacer;
 import quek.undergarden.world.gen.trunkplacer.SmogstemTrunkPlacer;
 
 public class UGTrunkPlacerTypes {
 
     public static final TrunkPlacerType<SmogstemTrunkPlacer> SMOGSTEM_TRUNK_PLACER = register("smogstem_trunk_placer", SmogstemTrunkPlacer.CODEC);
-    public static final TrunkPlacerType<GrongleTrunkPlacer> GRONGLE_TRUNK_PLACER = register("grongle_trunk_placer", GrongleTrunkPlacer.CODEC);
 
     private static <P extends TrunkPlacer> TrunkPlacerType<P> register(String name, Codec<P> codec) {
         return Registry.register(Registry.TRUNK_PLACER_TYPES, new ResourceLocation(Undergarden.MODID, name), new TrunkPlacerType<>(codec));
     }
-
-    public static void stupid() {}
 }
