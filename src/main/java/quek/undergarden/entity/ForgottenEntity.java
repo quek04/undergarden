a12ce5970e6320ffb8d96b0692a859ca82125436
quek <55364841+quek04@users.noreply.github.com>
2021-12-22 21:02:32 -0600

    begin forgotten entity class

---
+++ src/main/java/quek/undergarden/entity/ForgottenEntity.java
@@ -0,0 +1,78 @@
+package quek.undergarden.entity;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+
+public class ForgottenEntity extends AbstractSkeleton {
+
+    public ForgottenEntity(EntityType<? extends AbstractSkeleton> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.WITHER_SKELETON_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundEvents.WITHER_SKELETON_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.WITHER_SKELETON_DEATH;
+    }
+
+    @Override
+    protected SoundEvent getStepSound() {
+        return SoundEvents.WITHER_SKELETON_STEP;
+    }
+
+    @Override
+    protected void populateDefaultEquipmentSlots(DifficultyInstance difficulty) {
+        if (this.random.nextFloat() < 0.15F * difficulty.getSpecialMultiplier()) {
+            float f = 0.1F;
+
+            boolean flag = true;
+
+            for(EquipmentSlot equipmentslot : EquipmentSlot.values()) {
+                if (equipmentslot.getType() == EquipmentSlot.Type.ARMOR) {
+                    ItemStack itemstack = this.getItemBySlot(equipmentslot);
+                    if (!flag && this.random.nextFloat() < f) {
+                        break;
+                    }
+
+                    flag = false;
+                    if (itemstack.isEmpty()) {
+                        Item item = getEquipmentForSlot(equipmentslot);
+                        if (item != null) {
+                            this.setItemSlot(equipmentslot, new ItemStack(item));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Nullable
+    public static Item getEquipmentForSlot(EquipmentSlot slot) {
+        return switch (slot) {
+            case HEAD -> Items.NETHERITE_HELMET;
+            case CHEST -> Items.DIAMOND_CHESTPLATE;
+            case LEGS -> Items.DIAMOND_LEGGINGS;
+            default -> null;
+        };
+    }
+}
