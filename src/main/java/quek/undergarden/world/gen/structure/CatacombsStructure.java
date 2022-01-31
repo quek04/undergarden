42ccb732e38676f35fceab5b3a150cb29cb1a136
quek <55364841+quek04@users.noreply.github.com>
2022-01-30 20:51:57 -0600

    make forgotten spawn naturally in catacombs and remove spawners

--- src/main/java/quek/undergarden/world/gen/structure/CatacombsStructure.java
+++ src/main/java/quek/undergarden/world/gen/structure/CatacombsStructure.java
@@ -4,6 +4,9 @@ import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.random.WeightedRandomList;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
@@ -11,8 +14,14 @@ import net.minecraft.world.level.levelgen.feature.structures.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
 import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
 import quek.undergarden.Undergarden;
+import quek.undergarden.registry.UGEntityTypes;
+
+import java.util.Collections;
+import java.util.List;
 
 public class CatacombsStructure extends StructureFeature<JigsawConfiguration> {
+
+    public static final WeightedRandomList<MobSpawnSettings.SpawnerData> FORGOTTEN_SPAWNS = WeightedRandomList.create(new MobSpawnSettings.SpawnerData(UGEntityTypes.FORGOTTEN.get(), 100, 1, 1));
 
     public CatacombsStructure(Codec<JigsawConfiguration> codec) {
         super(codec, (context) -> {
@@ -31,5 +40,18 @@ public class CatacombsStructure extends StructureFeature<JigsawConfiguration> {
     @Override
     public String getFeatureName() {
         return "undergarden:catacombs";
+    }
+
+    @Override
+    public List<MobSpawnSettings.SpawnerData> getDefaultSpawnList(MobCategory category) {
+        if(category == MobCategory.MONSTER) {
+            return FORGOTTEN_SPAWNS.unwrap();
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean getDefaultRestrictsSpawnsToInside() {
+        return true;
     }
 }
