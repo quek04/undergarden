ea58a35546ba418d96fdb4e9299904ba6e0556bf
quek <55364841+quek04@users.noreply.github.com>
2022-02-04 15:39:20 -0600

    updated & removed useless code from DroopvineFeature

--- src/main/java/quek/undergarden/world/gen/feature/DroopvineFeature.java
+++ src/main/java/quek/undergarden/world/gen/feature/DroopvineFeature.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
-import quek.undergarden.block.DroopvinePlantBlock;
+import quek.undergarden.block.Droopvine;
 import quek.undergarden.registry.UGBlocks;
 
 import java.util.Random;
@@ -29,50 +29,28 @@ public class DroopvineFeature extends Feature<NoneFeatureConfiguration> {
         WorldGenLevel level = pContext.level();
         BlockPos pos = pContext.origin();
         Random random = pContext.random();
-        if (!level.isEmptyBlock(pos)) {
+        if(!level.isEmptyBlock(pos)) {
             return false;
-        } else {
+        }
+        else {
             BlockState blockstate = level.getBlockState(pos.above());
-            if (!blockstate.is(UGBlocks.DEPTHROCK.get()) && !blockstate.is(UGBlocks.SHIVERSTONE.get())) {
+            if(!blockstate.is(UGBlocks.DEPTHROCK.get()) && !blockstate.is(UGBlocks.SHIVERSTONE.get())) {
                 return false;
-            } else {
-                this.placeRoofNetherWart(level, random, pos);
+            }
+            else {
                 this.placeRoofDroopvine(level, random, pos);
                 return true;
             }
         }
     }
 
-    private void placeRoofNetherWart(LevelAccessor world, Random rand, BlockPos pos) {
-        BlockPos.MutableBlockPos blockpos$mutable = new BlockPos.MutableBlockPos();
-        BlockPos.MutableBlockPos blockpos$mutable1 = new BlockPos.MutableBlockPos();
-
-        for(int i = 0; i < 200; ++i) {
-            blockpos$mutable.setWithOffset(pos, rand.nextInt(6) - rand.nextInt(6), rand.nextInt(2) - rand.nextInt(5), rand.nextInt(6) - rand.nextInt(6));
-            if (world.isEmptyBlock(blockpos$mutable)) {
-                int j = 0;
-
-                for(Direction direction : directionArray) {
-                    BlockState blockstate = world.getBlockState(blockpos$mutable1.setWithOffset(blockpos$mutable, direction));
-                    if (blockstate.is(UGBlocks.DEPTHROCK.get()) || blockstate.is(UGBlocks.SHIVERSTONE.get())) {
-                        ++j;
-                    }
-
-                    if (j > 1) {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
     private void placeRoofDroopvine(LevelAccessor world, Random rand, BlockPos pos) {
-        BlockPos.MutableBlockPos blockpos$mutable = new BlockPos.MutableBlockPos();
+        BlockPos.MutableBlockPos posMutable = new BlockPos.MutableBlockPos();
 
         for(int i = 0; i < 100; ++i) {
-            blockpos$mutable.setWithOffset(pos, rand.nextInt(8) - rand.nextInt(8), rand.nextInt(2) - rand.nextInt(7), rand.nextInt(8) - rand.nextInt(8));
-            if (world.isEmptyBlock(blockpos$mutable)) {
-                BlockState blockstate = world.getBlockState(blockpos$mutable.above());
+            posMutable.setWithOffset(pos, rand.nextInt(8) - rand.nextInt(8), rand.nextInt(2) - rand.nextInt(7), rand.nextInt(8) - rand.nextInt(8));
+            if (world.isEmptyBlock(posMutable)) {
+                BlockState blockstate = world.getBlockState(posMutable.above());
                 if (blockstate.is(UGBlocks.DEPTHROCK.get()) || blockstate.is(UGBlocks.SHIVERSTONE.get())) {
                     int length = Mth.nextInt(rand, 1, 8);
                     if (rand.nextInt(6) == 0) {
@@ -83,7 +61,7 @@ public class DroopvineFeature extends Feature<NoneFeatureConfiguration> {
                         length = 1;
                     }
 
-                    placeDroopvineColumn(world, rand, blockpos$mutable, length, 17, 25);
+                    placeDroopvineColumn(world, rand, posMutable, length, 17, 25);
                 }
             }
         }
@@ -93,11 +71,11 @@ public class DroopvineFeature extends Feature<NoneFeatureConfiguration> {
         for(int i = 0; i <= length; ++i) {
             if (world.isEmptyBlock(posMutable)) {
                 if (i == length || !world.isEmptyBlock(posMutable.below())) {
-                    world.setBlock(posMutable, UGBlocks.DROOPVINE.get().defaultBlockState().setValue(GrowingPlantHeadBlock.AGE, Mth.nextInt(rand, min, max)), 2);
+                    world.setBlock(posMutable, UGBlocks.DROOPVINE.get().defaultBlockState().setValue(Droopvine.GLOWY, world.getRandom().nextBoolean()).setValue(GrowingPlantHeadBlock.AGE, Mth.nextInt(rand, min, max)), 2);
                     break;
                 }
 
-                world.setBlock(posMutable, UGBlocks.DROOPVINE_PLANT.get().defaultBlockState().setValue(DroopvinePlantBlock.GLOWY, world.getRandom().nextBoolean()), 2);
+                world.setBlock(posMutable, UGBlocks.DROOPVINE_PLANT.get().defaultBlockState().setValue(Droopvine.GLOWY, world.getRandom().nextBoolean()), 2);
             }
 
             posMutable.move(Direction.DOWN);
