d062929a17350c385cbea11c1ed0caf40110d162
quek <55364841+quek04@users.noreply.github.com>
2022-04-02 15:27:44 -0500

    added canEnchant and removed isTreasureOnly in ricochet enchantment

--- src/main/java/quek/undergarden/item/enchantment/RicochetEnchantment.java
+++ src/main/java/quek/undergarden/item/enchantment/RicochetEnchantment.java
@@ -1,8 +1,10 @@
 package quek.undergarden.item.enchantment;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import quek.undergarden.item.tool.slingshot.SlingshotItem;
 
 public class RicochetEnchantment extends Enchantment {
 
@@ -11,13 +13,13 @@ public class RicochetEnchantment extends Enchantment {
     }
 
     @Override
-    public int getMaxLevel() {
-        return 3;
+    public boolean canEnchant(ItemStack stack) {
+        return stack.getItem() instanceof SlingshotItem || super.canEnchant(stack);
     }
 
     @Override
-    public boolean isTreasureOnly() {
-        return true;
+    public int getMaxLevel() {
+        return 3;
     }
 
     @Override
@@ -27,11 +29,6 @@ public class RicochetEnchantment extends Enchantment {
 
     @Override
     public boolean isDiscoverable() {
-        return false;
-    }
-
-    @Override
-    public boolean isAllowedOnBooks() {
         return false;
     }
 }
