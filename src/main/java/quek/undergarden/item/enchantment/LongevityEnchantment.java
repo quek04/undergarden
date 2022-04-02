a0ec0bc7848221522f2aa904b76539b64a062ba1
quek <55364841+quek04@users.noreply.github.com>
2022-04-02 15:31:29 -0500

    added base longevity class and made ricochet uncommon

---
+++ src/main/java/quek/undergarden/item/enchantment/LongevityEnchantment.java
@@ -0,0 +1,34 @@
+package quek.undergarden.item.enchantment;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import quek.undergarden.item.tool.slingshot.SlingshotItem;
+
+public class LongevityEnchantment extends Enchantment {
+
+    public LongevityEnchantment(Rarity rarity, EnchantmentCategory category, EquipmentSlot... slots) {
+        super(rarity, category, slots);
+    }
+
+    @Override
+    public boolean canEnchant(ItemStack stack) {
+        return stack.getItem() instanceof SlingshotItem || super.canEnchant(stack);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 3;
+    }
+
+    @Override
+    public boolean isTradeable() {
+        return false;
+    }
+
+    @Override
+    public boolean isDiscoverable() {
+        return false;
+    }
+}
--- src/main/java/quek/undergarden/registry/UGEnchantments.java
+++ src/main/java/quek/undergarden/registry/UGEnchantments.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import quek.undergarden.Undergarden;
+import quek.undergarden.item.enchantment.LongevityEnchantment;
 import quek.undergarden.item.enchantment.RicochetEnchantment;
 import quek.undergarden.item.tool.slingshot.SlingshotItem;
 
@@ -16,5 +17,6 @@ public class UGEnchantments {
 
     public static final EnchantmentCategory SLINGSHOT = EnchantmentCategory.create("slingshot", (item -> item instanceof SlingshotItem));
 
-    public static final RegistryObject<Enchantment> RICOCHET = ENCHANTMENTS.register("ricochet", () -> new RicochetEnchantment(Enchantment.Rarity.COMMON, SLINGSHOT, EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> RICOCHET = ENCHANTMENTS.register("ricochet", () -> new RicochetEnchantment(Enchantment.Rarity.UNCOMMON, SLINGSHOT, EquipmentSlot.MAINHAND));
+    public static final RegistryObject<Enchantment> LONGEVITY = ENCHANTMENTS.register("longevity", () -> new LongevityEnchantment(Enchantment.Rarity.COMMON, SLINGSHOT, EquipmentSlot.MAINHAND));
 }
