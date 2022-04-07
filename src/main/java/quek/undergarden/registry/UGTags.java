65d5a60c211925662c452b070ce1c04918acf44d
GizmoTheMoonPig <cnicholasb@comcast.net>
2022-04-07 10:05:02 -0600

    add ores and grounds to forge tags

--- src/main/java/quek/undergarden/registry/UGTags.java
+++ src/main/java/quek/undergarden/registry/UGTags.java
@@ -75,6 +75,15 @@ public class UGTags {
         public static final TagKey<Block> STORAGE_BLOCKS_RAW_CLOGGRUM = forgeTag("storage_blocks/raw_cloggrum");
         public static final TagKey<Block> STORAGE_BLOCKS_RAW_FROSTSTEEL = forgeTag("storage_blocks/raw_froststeel");
 
+        public static final TagKey<Block> DEPTHROCK_GROUND = forgeTag("ore_bearing_ground/depthrock");
+        public static final TagKey<Block> DEPTHROCK_ORES = forgeTag("ores_in_ground/depthrock");
+
+        public static final TagKey<Block> SHIVERSTONE_GROUND = forgeTag("ore_bearing_ground/shiverstone");
+        public static final TagKey<Block> SHIVERSTONE_ORES = forgeTag("ores_in_ground/shiverstone");
+
+        public static final TagKey<Block> TREMBLECRUST_GROUND = forgeTag("ore_bearing_ground/tremblecrust");
+        public static final TagKey<Block> TREMBLECRUST_ORES = forgeTag("ores_in_ground/tremblecrust");
+
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(Undergarden.MODID, name));
         }
