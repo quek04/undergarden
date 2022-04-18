f02ea62d9516c8ccc622b4816908322b55727f66
quek <55364841+quek04@users.noreply.github.com>
2022-04-18 18:01:08 -0500

    added Screem's new brute

--- src/main/java/quek/undergarden/entity/animal/BruteEntity.java
+++ src/main/java/quek/undergarden/entity/animal/BruteEntity.java
@@ -5,10 +5,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.TimeUtil;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.NeutralMob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -111,5 +108,10 @@ public class BruteEntity extends Animal implements NeutralMob {
     @Override
     public boolean canBeLeashed(Player player) {
         return false;
+    }
+
+    @Override
+    protected float getStandingEyeHeight(Pose pPose, EntityDimensions pSize) {
+        return 1.9F;
     }
 }
