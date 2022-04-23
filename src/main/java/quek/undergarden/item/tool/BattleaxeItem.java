4b5b283f6d19f74765b869d93a9cf04bb3d656fb
quek <55364841+quek04@users.noreply.github.com>
2022-04-23 13:58:38 -0500

    moved battleaxe knockback to attribute modifier, prevented knockback enchant from being applied to battleaxes

--- src/main/java/quek/undergarden/item/tool/BattleaxeItem.java
+++ src/main/java/quek/undergarden/item/tool/BattleaxeItem.java
@@ -1,11 +1,16 @@
 package quek.undergarden.item.tool;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import quek.undergarden.registry.UGItemGroups;
@@ -13,8 +18,11 @@ import quek.undergarden.registry.UGItems;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 
 public class BattleaxeItem extends SwordItem {
+
+    private static final UUID ATTACK_KNOCKBACK_UUID = UUID.fromString("20D3EB3F-226F-4325-873E-9B0932E4E5C6");
 
     public BattleaxeItem(Tier tier, int attackDamage, float attackSpeed) {
         super(tier, attackDamage, attackSpeed, new Properties()
@@ -26,23 +34,15 @@ public class BattleaxeItem extends SwordItem {
     }
 
     @Override
-    public void fillItemCategory(CreativeModeTab tab, NonNullList<ItemStack> list) {
-        if(allowdedIn(tab)) {
-            ItemStack stack = new ItemStack(this);
-            stack.enchant(Enchantments.KNOCKBACK, 4);
-            list.add(stack);
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        if (slot == EquipmentSlot.MAINHAND) {
+            return ImmutableMultimap.of(
+                    Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", super.getDamage(), AttributeModifier.Operation.ADDITION),
+                    Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", -3.4F, AttributeModifier.Operation.ADDITION),
+                    Attributes.ATTACK_KNOCKBACK, new AttributeModifier(ATTACK_KNOCKBACK_UUID, "Weapon modifier", 4.0F, AttributeModifier.Operation.ADDITION)
+            );
         }
-    }
-
-    @Override
-    public void onCraftedBy(ItemStack stack, Level level, Player player) {
-        stack.serializeNBT();
-        stack.enchant(Enchantments.KNOCKBACK, 4);
-    }
-
-    @Override
-    public boolean isFoil(ItemStack stack) {
-        return false;
+        else return super.getAttributeModifiers(slot, stack);
     }
 
     @Override
@@ -50,5 +50,10 @@ public class BattleaxeItem extends SwordItem {
         if(stack.getItem() == UGItems.FORGOTTEN_BATTLEAXE.get()) {
             tooltip.add(new TranslatableComponent("tooltip.forgotten_sword").withStyle(ChatFormatting.GREEN));
         }
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        return enchantment.category.canEnchant(stack.getItem()) && enchantment != Enchantments.KNOCKBACK;
     }
 }
