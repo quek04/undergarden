24ce697be26c9bd8c6c08fc74f11ee43be02e36d
quek <55364841+quek04@users.noreply.github.com>
2022-05-21 19:25:50 -0500

    made boomgourds ignitable by ditchbulb paste

--- src/main/java/quek/undergarden/block/BoomgourdBlock.java
+++ src/main/java/quek/undergarden/block/BoomgourdBlock.java
@@ -4,13 +4,23 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.TntBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
 import quek.undergarden.entity.Boomgourd;
+import quek.undergarden.registry.UGItems;
 import quek.undergarden.registry.UGSoundEvents;
 
 import javax.annotation.Nullable;
@@ -38,6 +48,28 @@ public class BoomgourdBlock extends TntBlock {
             int fuse = boomgourd.getFuse();
             boomgourd.setFuse((short)(level.random.nextInt(fuse / 4) + fuse / 8));
             level.addFreshEntity(boomgourd);
+        }
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        ItemStack stack = player.getItemInHand(hand);
+        if (!stack.is(Items.FLINT_AND_STEEL) && !stack.is(Items.FIRE_CHARGE) && !stack.is(UGItems.DITCHBULB_PASTE.get())) {
+            return super.use(state, level, pos, player, hand, result);
+        } else {
+            onCaughtFire(state, level, pos, result.getDirection(), player);
+            level.setBlock(pos, Blocks.AIR.defaultBlockState(), 11);
+            Item item = stack.getItem();
+            if (!player.isCreative()) {
+                if (stack.is(Items.FLINT_AND_STEEL)) {
+                    stack.hurtAndBreak(1, player, (player1) -> player1.broadcastBreakEvent(hand));
+                } else {
+                    stack.shrink(1);
+                }
+            }
+
+            player.awardStat(Stats.ITEM_USED.get(item));
+            return InteractionResult.sidedSuccess(level.isClientSide);
         }
     }
 }
