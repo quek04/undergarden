cac5a7bcde6967cc9297743e6593a8b519ed0b5d
quek <55364841+quek04@users.noreply.github.com>
2022-05-30 13:18:37 -0500

    made planted gloomgourds able to place gloomgourds on blocks tagged as dirt

--- src/main/java/quek/undergarden/block/UGStemBlock.java
+++ src/main/java/quek/undergarden/block/UGStemBlock.java
@@ -3,6 +3,7 @@ package quek.undergarden.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
@@ -14,41 +15,41 @@ import java.util.Random;
 
 public class UGStemBlock extends StemBlock {
 
-    public UGStemBlock(StemGrownBlock pFruit, Properties pProperties) {
-        super(pFruit, UGItems.GLOOMGOURD_SEEDS, pProperties);
+    public UGStemBlock(StemGrownBlock block, Properties properties) {
+        super(block, UGItems.GLOOMGOURD_SEEDS, properties);
     }
 
     @Override
-    public boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
+    public boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
         return state.getBlock() == UGBlocks.DEEPSOIL_FARMLAND.get();
     }
 
     @Override
-    public boolean canSurvive(BlockState state, LevelReader worldIn, BlockPos pos) {
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
         BlockPos blockpos = pos.below();
-        return mayPlaceOn(worldIn.getBlockState(blockpos), worldIn, blockpos);
+        return mayPlaceOn(level.getBlockState(blockpos), level, blockpos);
     }
 
     @Override
-    public void randomTick(BlockState state, ServerLevel worldIn, BlockPos pos, Random random) {
-        if (!worldIn.isAreaLoaded(pos, 1))
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, Random random) {
+        if (!level.isAreaLoaded(pos, 1))
             return; // Forge: prevent loading unloaded chunks when checking neighbor's light
-        float f = CropBlock.getGrowthSpeed(this, worldIn, pos);
-        if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
+        float f = CropBlock.getGrowthSpeed(this, level, pos);
+        if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(level, pos, state, random.nextInt((int) (25.0F / f) + 1) == 0)) {
             int i = state.getValue(AGE);
             if (i < 7) {
-                worldIn.setBlock(pos, state.setValue(AGE, i + 1), 2);
+                level.setBlock(pos, state.setValue(AGE, i + 1), 2);
             } else {
                 Direction direction = Direction.Plane.HORIZONTAL.getRandomDirection(random);
                 BlockPos blockpos = pos.relative(direction);
-                BlockState blockstate = worldIn.getBlockState(blockpos.below());
+                BlockState blockstate = level.getBlockState(blockpos.below());
                 Block block = blockstate.getBlock();
-                if (worldIn.isEmptyBlock(blockpos) && (blockstate.canSustainPlant(worldIn, blockpos.below(), Direction.UP, this) || block == UGBlocks.DEEPSOIL_FARMLAND.get() || block == UGBlocks.DEEPSOIL.get() || block == UGBlocks.COARSE_DEEPSOIL.get() || block == UGBlocks.DEEPTURF_BLOCK.get())) {
-                    worldIn.setBlockAndUpdate(blockpos, this.getFruit().defaultBlockState());
-                    worldIn.setBlockAndUpdate(pos, this.getFruit().getAttachedStem().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, direction));
+                if (level.isEmptyBlock(blockpos) && (blockstate.canSustainPlant(level, blockpos.below(), Direction.UP, this) || blockstate.is(BlockTags.DIRT) || block == UGBlocks.DEEPSOIL_FARMLAND.get() || block == UGBlocks.DEEPSOIL.get() || block == UGBlocks.COARSE_DEEPSOIL.get() || block == UGBlocks.DEEPTURF_BLOCK.get())) {
+                    level.setBlockAndUpdate(blockpos, this.getFruit().defaultBlockState());
+                    level.setBlockAndUpdate(pos, this.getFruit().getAttachedStem().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, direction));
                 }
             }
-            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(level, pos, state);
         }
     }
 }
