a57abe2a858e6fe01a6c33825b945f814ba853e3
quek <55364841+quek04@users.noreply.github.com>
2022-10-16 11:48:51 -0500

    removed undergarden biomes from "is_" tags, minor version bump

--- src/main/java/quek/undergarden/data/UGBiomeTags.java
+++ src/main/java/quek/undergarden/data/UGBiomeTags.java
@@ -2,8 +2,6 @@ package quek.undergarden.data;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
-import net.minecraft.tags.BiomeTags;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 import quek.undergarden.Undergarden;
@@ -22,14 +20,14 @@ public class UGBiomeTags extends BiomeTagsProvider {
         tag(UGTags.Biomes.IS_UNDERGARDEN).add(UGBiomes.ANCIENT_SEA, UGBiomes.BARREN_ABYSS, UGBiomes.BLOOD_MUSHROOM_BOG, UGBiomes.DEAD_SEA, UGBiomes.DENSE_FOREST, UGBiomes.FORGOTTEN_FIELD, UGBiomes.FROSTFIELDS, UGBiomes.GRONGLEGROWTH, UGBiomes.ICY_SEA, UGBiomes.INDIGO_MUSHROOM_BOG, UGBiomes.INK_MUSHROOM_BOG, UGBiomes.SMOGSTEM_FOREST, UGBiomes.SMOG_SPIRES, UGBiomes.VEIL_MUSHROOM_BOG, UGBiomes.WIGGLEWOOD_FOREST);
         tag(UGTags.Biomes.HAS_CATACOMBS).add(UGBiomes.ANCIENT_SEA, UGBiomes.DENSE_FOREST, UGBiomes.FORGOTTEN_FIELD, UGBiomes.GRONGLEGROWTH, UGBiomes.SMOGSTEM_FOREST, UGBiomes.WIGGLEWOOD_FOREST);
 
-        //vanilla
-        tag(BiomeTags.IS_FOREST).add(UGBiomes.SMOGSTEM_FOREST, UGBiomes.WIGGLEWOOD_FOREST, UGBiomes.DENSE_FOREST, UGBiomes.GRONGLEGROWTH);
+        /*//vanilla
         tag(BiomeTags.WITHOUT_ZOMBIE_SIEGES).addTag(UGTags.Biomes.IS_UNDERGARDEN);
         tag(BiomeTags.WITHOUT_PATROL_SPAWNS).addTag(UGTags.Biomes.IS_UNDERGARDEN);
         tag(BiomeTags.WITHOUT_WANDERING_TRADER_SPAWNS).addTag(UGTags.Biomes.IS_UNDERGARDEN);
+        tag(BiomeTags.IS_FOREST).add(UGBiomes.SMOGSTEM_FOREST, UGBiomes.WIGGLEWOOD_FOREST, UGBiomes.DENSE_FOREST, UGBiomes.GRONGLEGROWTH);
 
         //forge
-        tag(Tags.Biomes.IS_COLD).add(UGBiomes.FROSTFIELDS, UGBiomes.ICY_SEA, UGBiomes.VEIL_MUSHROOM_BOG);
+        tag(Tags.Biomes.IS_COLD).add(UGBiomes.FROSTFIELDS, UGBiomes.ICY_SEA);
         tag(Tags.Biomes.IS_SPARSE).add(UGBiomes.FORGOTTEN_FIELD, UGBiomes.FROSTFIELDS, UGBiomes.BARREN_ABYSS);
         tag(Tags.Biomes.IS_DENSE).add(UGBiomes.DENSE_FOREST, UGBiomes.GRONGLEGROWTH);
         tag(Tags.Biomes.IS_DRY).add(UGBiomes.BARREN_ABYSS, UGBiomes.SMOG_SPIRES);
@@ -37,7 +35,7 @@ public class UGBiomeTags extends BiomeTagsProvider {
         tag(Tags.Biomes.IS_WATER).add(UGBiomes.ANCIENT_SEA, UGBiomes.ICY_SEA, UGBiomes.DEAD_SEA);
         tag(Tags.Biomes.IS_PLAINS).add(UGBiomes.FORGOTTEN_FIELD, UGBiomes.FROSTFIELDS);
         tag(Tags.Biomes.IS_SWAMP).add(UGBiomes.BLOOD_MUSHROOM_BOG, UGBiomes.INDIGO_MUSHROOM_BOG, UGBiomes.INK_MUSHROOM_BOG, UGBiomes.VEIL_MUSHROOM_BOG);
-        tag(Tags.Biomes.IS_WASTELAND).add(UGBiomes.BARREN_ABYSS, UGBiomes.DEAD_SEA);
+        tag(Tags.Biomes.IS_WASTELAND).add(UGBiomes.BARREN_ABYSS, UGBiomes.DEAD_SEA);*/
     }
 
     @Override
