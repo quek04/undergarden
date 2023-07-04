76e3f6e5d0ef1dcc513ee1ab14dd1c14b5ac9c10
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-07-03 21:47:09 -0600

    boomgourds can now be dispensed

--- src/main/java/quek/undergarden/Undergarden.java
+++ src/main/java/quek/undergarden/Undergarden.java
@@ -19,6 +19,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.BucketItem;
@@ -29,6 +30,7 @@ import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.properties.WoodType;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -44,6 +46,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import quek.undergarden.data.*;
+import quek.undergarden.entity.Boomgourd;
 import quek.undergarden.entity.projectile.Blisterbomb;
 import quek.undergarden.entity.projectile.slingshot.*;
 import quek.undergarden.item.tool.slingshot.AbstractSlingshotAmmoBehavior;
@@ -156,6 +159,20 @@ public class Undergarden {
 			DispenserBlock.registerBehavior(UGBlocks.GRONGLET.get(), new AbstractProjectileDispenseBehavior() {
 				protected Projectile getProjectile(Level level, Position position, ItemStack stack) {
 					return Util.make(new Gronglet(level, position.x(), position.y(), position.z()), (entity) -> entity.setItem(stack));
+				}
+			});
+
+			DispenserBlock.registerBehavior(UGBlocks.BOOMGOURD.get(), new DefaultDispenseItemBehavior() {
+
+				protected ItemStack execute(BlockSource source, ItemStack stack) {
+					Level level = source.getLevel();
+					BlockPos blockpos = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
+					Boomgourd gourd = new Boomgourd(level, (double) blockpos.getX() + 0.5D, blockpos.getY(), (double) blockpos.getZ() + 0.5D, null);
+					level.addFreshEntity(gourd);
+					level.playSound(null, gourd.getX(), gourd.getY(), gourd.getZ(), UGSoundEvents.BOOMGOURD_PRIMED.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
+					level.gameEvent(null, GameEvent.ENTITY_PLACE, blockpos);
+					stack.shrink(1);
+					return stack;
 				}
 			});
 
