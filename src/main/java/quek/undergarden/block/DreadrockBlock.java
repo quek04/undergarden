93c397a70b32e876d61d47d1b702f5b59f0c276d
quek <55364841+quek04@users.noreply.github.com>
2024-02-18 13:48:02 -0600

    made dreadrock mine slow instead of being unbreakable without forgotten pickaxe

--- src/main/java/quek/undergarden/block/DreadrockBlock.java
+++ src/main/java/quek/undergarden/block/DreadrockBlock.java
@@ -15,12 +15,12 @@ public class DreadrockBlock extends Block {
     @Override
     public float getDestroyProgress(BlockState state, Player player, BlockGetter level, BlockPos pos) {
         float destroySpeed = state.getDestroySpeed(level, pos);
+        int i = net.minecraftforge.common.ForgeHooks.isCorrectToolForDrops(state, player) ? 30 : 100;
         if (destroySpeed == -1.0F) {
             return 0.0F;
         } else if (!(player.getMainHandItem().getItem() == UGItems.FORGOTTEN_PICKAXE.get())) {
-            return 0.0F;
+            return player.getDigSpeed(state, pos) / (destroySpeed * 64.0F) / (float)i;
         } else {
-            int i = net.minecraftforge.common.ForgeHooks.isCorrectToolForDrops(state, player) ? 30 : 100;
             return player.getDigSpeed(state, pos) / destroySpeed / (float)i;
         }
     }
