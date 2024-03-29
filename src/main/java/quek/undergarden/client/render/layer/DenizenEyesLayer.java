f7603fe71e4f43b7662c97e60d3fa84d3ce6ee20
quek <55364841+quek04@users.noreply.github.com>
2024-03-29 01:07:38 -0500

    fixed tall denizen eyes

--- src/main/java/quek/undergarden/client/render/layer/DenizenEyesLayer.java
+++ src/main/java/quek/undergarden/client/render/layer/DenizenEyesLayer.java
@@ -1,21 +1,31 @@
 package quek.undergarden.client.render.layer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.client.renderer.entity.layers.EyesLayer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import quek.undergarden.Undergarden;
 import quek.undergarden.entity.monster.Denizen;
 
-public class DenizenEyesLayer<T extends Denizen, M extends EntityModel<T>> extends EyesLayer<T, M> {
+public class DenizenEyesLayer<T extends Denizen, M extends EntityModel<T>> extends RenderLayer<T, M> {
+	private static final RenderType DENIZEN_EYES = RenderType.eyes(new ResourceLocation(Undergarden.MODID, "textures/entity/denizen_eyes.png"));
+	private static final RenderType DENIZEN2_EYES = RenderType.eyes(new ResourceLocation(Undergarden.MODID, "textures/entity/denizen2_eyes.png"));;
 
 	public DenizenEyesLayer(RenderLayerParent<T, M> parent) {
 		super(parent);
 	}
 
 	@Override
-	public RenderType renderType() {
-		return RenderType.eyes(new ResourceLocation(Undergarden.MODID, "textures/entity/denizen_eyes.png"));
+	public void render(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+		VertexConsumer consumer = buffer.getBuffer(switch (entity.getVariant()) {
+			case SHORT -> DENIZEN_EYES;
+			case TALL -> DENIZEN2_EYES;
+		});
+		this.getParentModel().renderToBuffer(poseStack, consumer, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
 	}
 }
