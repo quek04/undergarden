684782e9072fa7e037fbcba67d6f455d802b68e2
quek <55364841+quek04@users.noreply.github.com>
2024-04-04 20:03:52 -0500

    made totem check happen every second again but now the particles dont want to work ??

--- src/main/java/quek/undergarden/block/entity/DenizenTotemBlockEntity.java
+++ src/main/java/quek/undergarden/block/entity/DenizenTotemBlockEntity.java
@@ -22,17 +22,16 @@ public class DenizenTotemBlockEntity extends BlockEntity {
 	}
 
 	public static void tick(Level level, BlockPos pos, BlockState state, DenizenTotemBlockEntity blockEntity) {
-		List<LivingEntity> entityList = level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(5.0D));
-		if (!entityList.isEmpty()) {
-			level.setBlockAndUpdate(pos, state.setValue(DenizenTotemBlock.ACTIVE, true));
-			entityList.forEach(entity -> {
-				if (!entity.hasEffect(UGEffects.PURITY.get())) {
-					entity.addEffect(new MobEffectInstance(UGEffects.PURITY.get(), 200, 0, true, true));
+		if (level.getGameTime() % 20 == 0) {
+			List<LivingEntity> entityList = level.getEntitiesOfClass(LivingEntity.class, new AABB(pos).inflate(5.0D));
+			if (!entityList.isEmpty()) {
+				level.setBlockAndUpdate(pos, state.setValue(DenizenTotemBlock.ACTIVE, true));
+                entityList.stream().filter(entity -> !entity.hasEffect(UGEffects.PURITY.get())).forEach(entity -> {
+                    entity.addEffect(new MobEffectInstance(UGEffects.PURITY.get(), 200, 0, true, true));
 					drawParticlesTo(level, pos.getCenter(), entity);
-				}
-
-			});
-		} else level.setBlockAndUpdate(pos, state.setValue(DenizenTotemBlock.ACTIVE, false));
+                });
+			} else level.setBlockAndUpdate(pos, state.setValue(DenizenTotemBlock.ACTIVE, false));
+		}
 	}
 
 	private static void drawParticlesTo(Level level, Vec3 totemPos, Entity highlight) {
@@ -40,9 +39,8 @@ public class DenizenTotemBlockEntity extends BlockEntity {
 		for (int i = 0; i < particles; i++) {
 			double trailFactor = i / (particles - 1.0D);
 			double x = totemPos.x() + (highlight.position().x() - totemPos.x()) * trailFactor + level.getRandom().nextFloat() * 0.25D;
-			double y = totemPos.y() + (highlight.getEyePosition().y() - totemPos.y()) * trailFactor + level.getRandom().nextFloat() * 0.25D;
+			double y = totemPos.y() + (highlight.getEyeY() - totemPos.y()) * trailFactor + level.getRandom().nextFloat() * 0.25D;
 			double z = totemPos.z() + (highlight.position().z() - totemPos.z()) * trailFactor + level.getRandom().nextFloat() * 0.25D;
-			//level.sendParticles(UGParticleTypes.ROGDORIUM_SPARKLE.get(), x, y, z, 1, 0, 0, 0, 0);
 			level.addParticle(UGParticleTypes.ROGDORIUM_SPARKLE.get(), x, y, z, 0, 0, 0);
 		}
 	}
