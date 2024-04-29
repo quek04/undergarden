3a3a44cba551f75fe245ba2dd2f8ec47c857efc6
quek <55364841+quek04@users.noreply.github.com>
2024-04-29 17:12:58 -0500

    fixed particle packet

--- src/main/java/quek/undergarden/network/CreateCritParticlePacket.java
+++ src/main/java/quek/undergarden/network/CreateCritParticlePacket.java
@@ -2,7 +2,6 @@ package quek.undergarden.network;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
@@ -13,19 +12,19 @@ import net.neoforged.neoforge.network.handling.IPayloadContext;
 import quek.undergarden.Undergarden;
 
 public record CreateCritParticlePacket(int entityID, int duration,
-									   ParticleType<?> particle) implements CustomPacketPayload {
+									   ParticleOptions particle) implements CustomPacketPayload {
 
 	public static final Type<CreateCritParticlePacket> TYPE = new Type<>(new ResourceLocation(Undergarden.MODID, "create_crit_particle"));
 	public static final StreamCodec<RegistryFriendlyByteBuf, CreateCritParticlePacket> STREAM_CODEC = CustomPacketPayload.codec(CreateCritParticlePacket::write, CreateCritParticlePacket::new);
 
 	public CreateCritParticlePacket(RegistryFriendlyByteBuf buf) {
-		this(buf.readInt(), buf.readInt(), (ParticleType<?>) ParticleTypes.STREAM_CODEC.decode(buf));
+		this(buf.readInt(), buf.readInt(), ParticleTypes.STREAM_CODEC.decode(buf));
 	}
 
 	public void write(RegistryFriendlyByteBuf buf) {
 		buf.writeInt(this.entityID());
 		buf.writeInt(this.duration());
-		ParticleTypes.STREAM_CODEC.encode(buf, this::particle);
+		ParticleTypes.STREAM_CODEC.encode(buf, this.particle());
 	}
 
 	@Override
@@ -37,7 +36,7 @@ public record CreateCritParticlePacket(int entityID, int duration,
 		ctx.enqueueWork(() -> {
 			Entity entity = Minecraft.getInstance().level.getEntity(message.entityID());
 			if (entity != null) {
-				Minecraft.getInstance().particleEngine.createTrackingEmitter(entity, (ParticleOptions) message.particle(), message.duration());
+				Minecraft.getInstance().particleEngine.createTrackingEmitter(entity, message.particle(), message.duration());
 			}
 		});
 	}
