00b4fa930425b6399ec2856b8e7e8b63855e69e4
quek <55364841+quek04@users.noreply.github.com>
2025-06-20 21:47:53 -0500

    buffed purity effect

--- src/main/java/quek/undergarden/effect/PurityEffect.java
+++ src/main/java/quek/undergarden/effect/PurityEffect.java
@@ -3,9 +3,7 @@ package quek.undergarden.effect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.network.PacketDistributor;
 import quek.undergarden.event.UthericInfectionEvents;
-import quek.undergarden.network.UthericInfectionPacket;
 import quek.undergarden.registry.UGAttachments;
 
 public class PurityEffect extends MobEffect {
@@ -15,10 +13,10 @@ public class PurityEffect extends MobEffect {
 
 	@Override
 	public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-		if (entity.tickCount % (400 / (amplifier + 1)) == 0 && !entity.level().isClientSide()) {
+		if (entity.tickCount % (200 / (amplifier + 1)) == 0 && !entity.level().isClientSide()) {
 			double data = entity.getData(UGAttachments.UTHERIC_INFECTION);
 			if (data > 0) {
-				entity.setData(UGAttachments.UTHERIC_INFECTION.get(), data - 1);
+				entity.setData(UGAttachments.UTHERIC_INFECTION.get(), data - (amplifier + 1));
 				UthericInfectionEvents.sendInfectionSyncPacket(entity);
 			}
 		}
