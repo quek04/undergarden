c0000ed2c01e05af554fedae692db902ed844815
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2026-03-31 20:50:15 -0600

    fix battleaxe holding anim in offhand

--- src/main/java/quek/undergarden/client/model/animation/BattleaxeAnimation.java
+++ src/main/java/quek/undergarden/client/model/animation/BattleaxeAnimation.java
@@ -1,5 +1,6 @@
 package quek.undergarden.client.model.animation;
 
+import net.minecraft.client.model.AnimationUtils;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
@@ -13,29 +14,40 @@ public class BattleaxeAnimation {
 		return Mth.clamp(Mth.inverseLerp(time, start, end), 0.0F, 1.0F);
 	}
 
+	//TODO swing main hand like normal when holding axe in offhand. Axe should not swing at all when in offhand
 	public static void thirdPersonAttackHand(HumanoidModel<?> model, HumanoidRenderState entity, HumanoidArm arm) {
 		float attackTime = entity.attackTime;
 
-		boolean holdingInRightArm = entity.mainArm == HumanoidArm.RIGHT;
+		boolean holdingInRightArm = entity.mainArm == arm;
 		ModelPart mainArm = holdingInRightArm ? model.rightArm : model.leftArm;
 		ModelPart otherArm = holdingInRightArm ? model.leftArm : model.rightArm;
-		float prepare = Ease.inOutSine(progress(attackTime, 0.0F, 0.05F));
-		float attack = Ease.inQuad(progress(attackTime, 0.05F, 0.3F));
-		float retract = Ease.inQuad(progress(attackTime, 0.3F, 1.0F));
 
-		float animationRot = (90.0F * prepare - 120.0F * attack + 30.0F * retract) * ((float) Math.PI / 180F);
-		float animationOtherHandRot = (90.0F * prepare + 30.0F * attack - 120.0F * retract) * ((float) Math.PI / 180F);
+		if (entity.attackArm == entity.mainArm) {
+			float prepare = Ease.inOutSine(progress(attackTime, 0.0F, 0.05F));
+			float attack = Ease.inQuad(progress(attackTime, 0.05F, 0.3F));
+			float retract = Ease.inQuad(progress(attackTime, 0.3F, 1.0F));
 
-		//It's used to recreate the momentum of a swing animation.
-		float animationSwingingRot = (120.0F * attack - 120.0F * retract) * ((float) Math.PI / 180F);
-		float animationOtherHandSwingingRot = (120.0F * prepare - 120.0F * retract) * ((float) Math.PI / 180F);
+			float animationRot = (90.0F * prepare - 120.0F * attack + 30.0F * retract) * Mth.DEG_TO_RAD;
+			float animationOtherHandRot = (90.0F * prepare + 30.0F * attack - 120.0F * retract) * Mth.DEG_TO_RAD;
 
-		mainArm.xRot = -1.3F - animationRot * 0.6F + animationSwingingRot * 0.6F;
-		mainArm.yRot = holdingInRightArm ? 0.3F : -0.3F;
-		mainArm.zRot = holdingInRightArm ? -1.0F - animationSwingingRot * 0.2F : 1.0F + animationSwingingRot * 0.2F;
+			//It's used to recreate the momentum of a swing animation.
+			float animationSwingingRot = (120.0F * attack - 120.0F * retract) * Mth.DEG_TO_RAD;
+			float animationOtherHandSwingingRot = (120.0F * prepare - 120.0F * retract) * Mth.DEG_TO_RAD;
 
-		otherArm.xRot = -0.5F - animationRot * 0.3F - animationOtherHandRot * 0.3F + animationOtherHandSwingingRot * 0.8F;
-		otherArm.yRot = holdingInRightArm ? 0.7F + animationRot * 0.2F : -0.7F - animationRot * 0.2F;
+			mainArm.xRot = -1.3F - animationRot * 0.6F + animationSwingingRot * 0.6F;
+			mainArm.zRot = holdingInRightArm ? -1.0F - animationSwingingRot * 0.2F : 1.0F + animationSwingingRot * 0.2F;
 
+			otherArm.xRot = -0.5F - animationRot * 0.3F - animationOtherHandRot * 0.3F + animationOtherHandSwingingRot * 0.8F;
+			otherArm.yRot = holdingInRightArm ? 0.7F + animationRot * 0.2F : -0.7F - animationRot * 0.2F;
+		} else {
+			mainArm.xRot = -1.3F;
+			mainArm.yRot = holdingInRightArm ? 0.3F : -0.3F;
+			mainArm.zRot = holdingInRightArm ? -1.0F : 1.0F;
+			otherArm.xRot = -0.5F;
+			otherArm.yRot = holdingInRightArm ? 0.7F : -0.7F;
+		}
+
+		AnimationUtils.bobModelPart(model.rightArm, entity.ageInTicks, -1.0F);
+		AnimationUtils.bobModelPart(model.leftArm, entity.ageInTicks, 1.0F);
 	}
 }
