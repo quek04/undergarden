b1ee5902e94a9a8aa2fb8a15d685ee4a7059a79a
Fishcookie <therealbutterman054@gmail.com>
2026-05-12 20:01:36 -0600

    bumass modder has artist fix his mod

--- src/main/java/quek/undergarden/registry/UGItemTiers.java
+++ src/main/java/quek/undergarden/registry/UGItemTiers.java
@@ -6,8 +6,8 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 public class UGItemTiers {
 
-	public static final ToolMaterial CLOGGRUM = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 286, 6.0F, 3.0F, 8, UGTags.Items.CLOGGRUM_TOOL_MATERIALS);
-	public static final ToolMaterial FROSTSTEEL = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 575, 7.0F, 2.0F, 20, UGTags.Items.FROSTSTEEL_TOOL_MATERIALS);
-	public static final ToolMaterial UTHERIUM = new ToolMaterial(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, 1279, 8.5F, 3.5F, 17, UGTags.Items.UTHERIC_TOOL_MATERIALS);
-	public static final ToolMaterial FORGOTTEN = new ToolMaterial(UGTags.Blocks.INCORRECT_FOR_FORGOTTEN_TOOL, 1876, 8.0F, 3.0F, 2, UGTags.Items.FORGOTTEN_TOOL_MATERIALS);
+	public static final ToolMaterial CLOGGRUM = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 288, 6.0F, 3.0F, 6, UGTags.Items.CLOGGRUM_TOOL_MATERIALS);
+	public static final ToolMaterial FROSTSTEEL = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 2864, 5.0F, 2.0F, 18, UGTags.Items.FROSTSTEEL_TOOL_MATERIALS);
+	public static final ToolMaterial UTHERIUM = new ToolMaterial(UGTags.Blocks.INCORRECT_FOR_FORGOTTEN_TOOL, 1248, 10.0F, 4.0F, 10, UGTags.Items.UTHERIC_TOOL_MATERIALS);
+	public static final ToolMaterial FORGOTTEN = new ToolMaterial(UGTags.Blocks.INCORRECT_FOR_FORGOTTEN_TOOL, 1872, 8.0F, 3.0F, 12, UGTags.Items.FORGOTTEN_TOOL_MATERIALS);
 }
